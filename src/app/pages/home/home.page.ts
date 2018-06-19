18346ae9536e1994ec8f0df133b6089d0859c7dc
Akash <borad.akash@gmail.com>
2018-06-19 19:03:33 +0530

    fixed navigation issue

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'gi-page-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  constructor(
+    public router: Router
+  ) { }
+  goToDetail(menu: string) {
+    this.router.navigateByUrl(
+      `home/${menu})`
+    );
+  }
 }
