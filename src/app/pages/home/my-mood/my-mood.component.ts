967a07ce10c0d8239976ac343796a9ebc40e66af
borad.akash@gmail.com <borad.akash@gmail.com>
2018-08-29 17:56:56 +0530

    changed mood page flow

--- src/app/pages/home/my-mood/my-mood.component.ts
+++ src/app/pages/home/my-mood/my-mood.component.ts
@@ -1,41 +1,64 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Observable } from "rxjs";
 import * as fromRootStore from "../../../../girls-platform/state/";
 import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'gi-my-mood',
   templateUrl: './my-mood.component.html',
   styleUrls: ['./my-mood.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MyMoodComponent implements OnInit {
-  moodLevel: number;
   loading$: Observable<boolean>;
   isAuthorized$: Observable<boolean>;
   error$: Observable<any>;
   moodText: string;
+  currentStep: number = 1;
+  mood: string = "Anger";
+  moodLevel: string = "1";
+  picture: string = "anger.jpg";
+  isPublic: boolean = true;
   constructor(
-    private store: Store<fromRootStore.State>
+    private store: Store<fromRootStore.State>,
+    private router: Router,
+    private cd: ChangeDetectorRef
   ) {
     this.loading$ = this.store.select(fromRootStore.getPostMoodLoading);
     this.error$ = this.store.select(fromRootStore.getPostMoodError);
     this.isAuthorized$ = this.store.select(fromRootStore.getUserAuthenticated);
   }
-
+  ionViewWillEnter() {
+    this.currentStep = 1;
+    this.cd.markForCheck();
+  }
   ngOnInit() {
   }
 
   postFeed() {
+    //<gi-mood-image src="${this.picture}"></gi-mood-image>
     var model = {
       CourseClassId: 1,
-      PostText: "Mood " + this.moodText,
-      ExternalResourceUrl: ""
+      PostText: `Mood- ${this.mood} and Level: ${this.moodLevel} `,
+      ExternalResourceUrl: "",
+      IsPublic: this.isPublic
     }
-    console.log(model);
     this.store.dispatch(new fromRootStore.PostMood(model));
   }
   moodChanged(val) {
     this.moodText = val.detail;
+  }
+  nextStep() {
+    this.currentStep = this.currentStep + 1;
+  }
+  backStep() {
+    if (this.currentStep > 1) {
+      this.currentStep = this.currentStep - 1;
+    }
+    else {
+      this.router.navigateByUrl('/home');
+    }
   }
 
 }
