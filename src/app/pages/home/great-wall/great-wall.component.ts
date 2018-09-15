63b881c1c90b79e09bd8ec78dfa07185866043a8
Martin Lund <mrlund@hotmail.com>
2018-09-15 16:14:10 +0200

    added formatNodeGraphData(feed) method to great white wall

--- src/app/pages/home/great-wall/great-wall.component.ts
+++ src/app/pages/home/great-wall/great-wall.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from "rxjs";
-import { filter, map } from "rxjs/operators";
+import { filter, map, tap } from "rxjs/operators";
 import * as fromRootStore from "../../../../girls-platform/state/";
 import { Store } from '@ngrx/store';
 
@@ -21,6 +21,63 @@ export class GreatWallComponent implements OnInit {
     this.posts$ = this.store.select(fromRootStore.getAllPosts);
     this.loading$ = this.store.select(fromRootStore.getPostLoadLoading);
     this.isAuthorized$ = this.store.select(fromRootStore.getUserAuthenticated);
+
+    this.posts$.pipe(
+      tap(x => this.formatNodeGraphData(x))
+    ).subscribe();
+
+  }
+  
+  formatNodeGraphData(feed){
+    let distinctNodes = {};
+    let distinctLinks = {};
+    let result = { nodes: [], links: []};
+    console.log(feed);
+
+    // DEV code only - to populate Ids and names while they're returned from API 
+    // feed.forEach(post=>{
+    //   post.UserId = post.Name;
+    //   post.MiniAppId = "APP123";
+    //   post.MiniAppName = "Cool MiniApp";
+    //   post.Comments.forEach(comment => {
+    //     comment.UserId = comment.Name;
+    //   });
+    // });
+
+    feed.forEach(post=>{
+
+      //Add users to nodes
+      if (!distinctNodes[post.UserId]){
+        distinctNodes[post.UserId] = true;
+        result.nodes.push(this.newNode(post.UserId, post.Name, 1));
+      }
+      //Add apps to nodes
+      if (!distinctNodes[post.MiniAppId]){
+        distinctNodes[post.MiniAppId] = true;
+        result.nodes.push(this.newNode(post.MiniAppId, post.MiniAppName, 2));
+      }
+      //Link users to apps
+      if (!distinctLinks[post.UserId + post.MiniAppId]){
+        distinctLinks[post.UserId + post.MiniAppId] = true;
+        result.links.push(this.newLink(post.MiniAppId, post.UserId, 2)); // Could "value" be 1 = user-to-app, 2 = user-to-user?
+      }
+
+      //Links users to other users 
+      post.Comments.forEach(comment => {
+        if (!distinctLinks[post.UserId + comment.UserId] && (!distinctLinks[comment.UserId + post.UserId])){
+          distinctLinks[post.UserId + comment.UserId] = true;
+          result.links.push(this.newLink(post.UserId, comment.UserId, 1)); 
+        }
+      });
+    });
+    console.log(result);
+    return result;
+  }
+  private newNode(id, name, group){
+    return { id: id, name: name, group: group, posx: 100, posy: 100, img: "assets/img/avatar.png" }
+  }
+  private newLink(source, target, value){
+    return { source: source, target: target, value: value };
   }
   nodeClick(item) {
     console.log(item);
